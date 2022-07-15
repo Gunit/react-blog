8919f1d81d6c523aa24367e15e512f72f36b5d6b
Danil Panarin <danilapanarin96@gmail.com>
2022-07-15 21:20:22 +0600

    add(fc)-del(fc)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Header from './components/Header/Header'
 import PostForm from './components/PostForm/PostForm'
 import PostList from './components/PostList/PostList'
@@ -5,12 +6,22 @@ import PostList from './components/PostList/PostList'
 import './style/App.css'
 
 const App = () => {
+	const [posts, setPosts] = useState([])
+
+	const createPost = post => {
+		post.id = Date.now()
+		setPosts([...posts, post])
+	}
+
+	const deletedPost = id => {
+		setPosts(posts.filter(p => p.id !== id))
+	}
 	return (
 		<div>
 			<Header />
 			<div className='wrapper'>
-				<PostForm />
-				<PostList />
+				<PostForm create={createPost} />
+				<PostList posts={posts} deleted={deletedPost} />
 			</div>
 		</div>
 	)
